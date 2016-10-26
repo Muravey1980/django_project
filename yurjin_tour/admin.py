7d23e79549ecee1c18245203e74ec0355273a750
Sergey Svobodsky <svobodsky@mail.ru>
2016-10-26 11:33:22 +0300

    yurjin_tour app test

--- yurjin_tour/admin.py
+++ yurjin_tour/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Country, City, Airport, Office, Manager, Tourist, Status, Contract
+
+admin.site.register(Country)
+admin.site.register(City)
+admin.site.register(Airport)
+admin.site.register(Office)
+admin.site.register(Manager)
+admin.site.register(Tourist)
+admin.site.register(Status)
+admin.site.register(Contract)
--- yurjin_tour/models.py
+++ yurjin_tour/models.py
@@ -3,10 +3,15 @@ from django.db import models
 # Create your models here.
 class Country(models.Model):
     country_name = models.CharField(max_length=200)
-
+    
+    def __str__(self):
+        return self.country_name
 
 class City(models.Model):
     city_name = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.city_name
     
 
 class Airport(models.Model):
@@ -14,10 +19,14 @@ class Airport(models.Model):
     iata_code = models.CharField(max_length=3)
     airport_name = models.CharField(max_length=200)
     #airport_city = models.CharField(max_length=200)
-
+    
+    def __str__(self):
+        return self.airport_name
+    
 
 class Office(models.Model):
     office_name = models.CharField(max_length=200)
+    office_adddress = models.CharField(max_length=200)
 
     def __str__(self):
         return self.office_name
@@ -32,11 +41,16 @@ class Manager(models.Model):
 
 
 class Tourist(models.Model):
-    tourist_name = models.CharField(max_length=200)
+    tourist_last_name = models.CharField(max_length=200)
+    tourist_first_name = models.CharField(max_length=200)
+    tourist_mid_name = models.CharField(max_length=200)
     tourist_passport = models.CharField(max_length=200)
+    tourist_international_name = models.CharField(max_length=200)
+    tourist_international_passport = models.CharField(max_length=200)
+    
     
     def __str__(self):
-        return self.tourist_name
+        return self.tourist_last_name + ' ' + self.tourist_first_name + ' ' + self.tourist_mid_name
     
     
 class Status(models.Model):
@@ -46,19 +60,22 @@ class Status(models.Model):
         return self.status_name
 
 class Contract(models.Model):
-    contract_num = models.IntegerField
+    contract_num = models.IntegerField()
     input_date = models.DateField(blank=True, null=True)
-    manager = models.ForeignKey(Manager, blank=True, null=True, on_delete=models.SET_NULL)
-    office = models.ForeignKey(Office, blank=True, null=True, on_delete=models.SET_NULL)
-    client = models.ForeignKey(Tourist, blank=True, null=True, on_delete=models.SET_NULL)
-    airport_from1 = models.ForeignKey(Airport, related_name='airport_from1', blank=True, null=True, on_delete=models.SET_NULL)
-    airport_to1 = models.ForeignKey(Airport, related_name='airport_to1', blank=True, null=True, on_delete=models.SET_NULL)
-    airport_from2 = models.ForeignKey(Airport, related_name='airport_from2', blank=True, null=True, on_delete=models.SET_NULL)
-    airport_to2 = models.ForeignKey(Airport, related_name='airport_to2', blank=True, null=True, on_delete=models.SET_NULL)
-    status = models.ForeignKey(Status, blank=True, null=True, on_delete=models.SET_NULL)
+    manager = models.ForeignKey(Manager, models.SET_NULL, blank=True, null=True)
+    office = models.ForeignKey(Office, models.SET_NULL, blank=True, null=True)
+    client = models.ForeignKey(Tourist, models.SET_NULL, blank=True, null=True)
+    airport_from1 = models.ForeignKey(Airport, models.SET_NULL, related_name='airport_from1', blank=True, null=True)
+    airport_to1 = models.ForeignKey(Airport, models.SET_NULL, related_name='airport_to1', blank=True, null=True)
+    airport_from2 = models.ForeignKey(Airport, models.SET_NULL, related_name='airport_from2', blank=True, null=True)
+    airport_to2 = models.ForeignKey(Airport, models.SET_NULL, related_name='airport_to2', blank=True, null=True)
+    status = models.ForeignKey(Status, models.SET_NULL, blank=True, null=True)
     conclusion_date = models.DateField()
-    signatory = models.ForeignKey(Manager, related_name='contract_signatory', blank=True, null=True, on_delete=models.SET_NULL)
+    signatory = models.ForeignKey(Manager, models.SET_NULL, related_name='contract_signatory', blank=True, null=True)
+    tourist_list = models.ManyToManyField(Tourist, related_name='tourist_list') 
+    
     
     def __str__(self):
-        return str(self.contract_num) + ' от ' + str(self.input_date)
+        return 'Договор №' + str(self.contract_num) + ' от ' + str(self.input_date)
+
     
