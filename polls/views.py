a6b2cac37f421fafbdd6c1d8c22d26445322e76d
Sergey Svobodsky <svobodsky@mail.ru>
2016-10-26 11:33:22 +0300

    yurjin_tour app added

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
-from django.http import HttpResponse
-from _datetime import date
-#, time, timezone
-
-
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
