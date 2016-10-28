e3f2faadd4bd3da773c4315cd4c7da74d0648a9f
Sergey Svobodsky <svobodsky@mail.ru>
2016-10-28 10:06:53 +0300

    merged

--- yurjin_tour/views.py
+++ yurjin_tour/views.py
@@ -4,13 +4,9 @@ from .models import Contract
 
 # Create your views here.
 def index(request):
-<<<<<<< Upstream, based on django_project/master
-    return HttpResponse("Hello, world. You're at the yurjin_tour index.")
-=======
     latest_contract_list = Contract.objects.order_by('-input_date')[:5]
     output = ', '.join([str(q.contract_num) for q in latest_contract_list])
     return HttpResponse(output)
->>>>>>> 1a09349 just edit
 
 
 def contract_detail(request, contract_id):
